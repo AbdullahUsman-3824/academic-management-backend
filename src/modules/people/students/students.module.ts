067207c3f9fce4316e1e40655888e2dd1254a14a
M. Abdullah Usman <abdullaasheikh3824@gmail.com>
2026-09-16 20:15:53 +0500

    feat(students): add UserService and RoleService to StudentsModule providers

--- src/modules/people/students/students.module.ts
+++ src/modules/people/students/students.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StudentsController } from './controllers/students.controller';
 import { StudentsService } from './services/students.service';
+import { UserService } from '../../user/user.service';
+import { RoleService } from '../../user/role/role.service';
 import { DatabaseModule } from '../../../database/database.module';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [StudentsController],
-  providers: [StudentsService],
+  providers: [StudentsService, UserService, RoleService],
   exports: [StudentsService],
 })
 export class StudentsModule {}
